fcf8fee622845dd02157c7491aa46e804e27e147
CarlosHerazo <Carlosherazo2004@gmail.com>
2024-11-19 02:06:22 -0500

    true

--- ruedaViva/settings.py
+++ ruedaViva/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-upm#5%%(fvb7*@x#!c9wbk2rf(*y!0j^@y+6%9a266xt#x7vua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
